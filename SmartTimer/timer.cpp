763c44c3297c76ab598653815813547c18fa8583
SergeiSanJj <isara.isara8@gmail.com>
2018-10-07 15:35:33 +0300

    Design Timer backend

--- SmartTimer/timer.cpp
+++ SmartTimer/timer.cpp
@@ -1,6 +1,35 @@
 #include "timer.h"
 
-Timer::Timer()
+Timer::Timer():
+    QObject(), timer(new QTimer()), interval(QTime(0,0,0)), isPaused(false)
 {
 
 }
+
+Timer::Timer(QTime timeInterval):
+    QObject(), timer(new QTimer()), interval(timeInterval), isPaused(false)
+{
+
+}
+
+Timer::~Timer()
+{
+    stop();
+    delete timer;
+}
+
+void Timer::start()
+{
+    timer->start(interval.elapsed());
+}
+
+void Timer::stop()
+{
+    timer->stop();
+}
+
+void Timer::pause()
+{
+    pausePassed = timer->remainingTime();
+}
+
--- SmartTimer/timer.h
+++ SmartTimer/timer.h
@@ -1,11 +1,37 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include <QTimer>
+#include <QTime>
+#include <QObject>
 
-class Timer
+class Timer: public QObject
 {
+    Q_OBJECT
 public:
     Timer();
+
+    Timer(QTime timeInterval);
+
+    ~Timer();
+
+public slots:
+    void start();
+    void stop();
+    void pause();
+signals:
+    void timerStarted();
+    void timerStoped();
+    void timerPaused();
+
+    void ring();
+
+private:
+    QTimer* timer;
+    QTime interval;
+    QTime started;
+    int pausePassed = 0;
+    bool isPaused;
 };
 
 #endif // TIMER_H
